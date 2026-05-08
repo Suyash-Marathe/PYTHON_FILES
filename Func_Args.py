eb2e172d5f5b21e67341e2fd601970141897bfaa
Suyash-Marathe <suyashmrth@gmail.com>
2026-05-08 22:19:59 +0530

    Add files via upload

---
+++ Func_Args.py
@@ -0,0 +1,15 @@
+def greeting(name, end) :
+
+      print("Good Morning", name, end)
+      return "Done"
+
+
+greeting("Suyash", "Thank you")
+greeting("Nitin", "Thank you")
+greeting("Rahul", "Thank you")
+greeting("Manish", "Thank you")
+greeting("Hitesh", "Thank you")
+greeting("Yash", "Thank you")
+
+a = greeting("Nilesh", "Thank you")
+print(a)
---
+++ Recursion.py
@@ -0,0 +1,10 @@
+
+n = int(input("Enter number :"))
+temp = n
+
+def fact(n) :
+
+      if (n == 1 or n == 0) : return 1
+      return n * fact(n - 1)
+
+print(f"Factorail of {temp} is {fact(n)}")
